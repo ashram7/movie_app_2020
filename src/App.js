115c996b1e17c8d2cbab68da9eee7c8271d75c85
ashram7 <ashram7@hanmail.net>
2022-03-04 17:10:34 +0900

    03 깃허브에 리액트 앱 업로드

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+function Fruit({ fav }){
+  return <h1>I like { fav }</h1>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div> 
+      <h1>Hello</h1>
+      <Fruit fav="banana" />
+      <Fruit fav="orange" />
+      <Fruit fav="apple" />
+      <Fruit fav="melon" />          
     </div>
-  );
+  );  
 }
 
 export default App;
