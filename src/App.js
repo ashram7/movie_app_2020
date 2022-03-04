1bbdfc3c5401eab536030229e9818907c970360f
ashram7 <ashram7@hanmail.net>
2022-03-04 19:38:18 +0900

    05 깃허브에 리액트 앱 업로드하기

--- src/App.js
+++ src/App.js
@@ -1,58 +1,21 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-function Fruit({ name, picture, rating }){
-  return (
-    <div>
-      <h2>I like { name }</h2>
-      <h4> {rating} / 5.0 </h4>
-      <img src= {picture} alt= {name} />
-    </div> 
-   );
+class App extends React.Component {
+  state = {
+    isLoading : true,
+    movies: [],
+  };
+  componentDidMount() {
+    // 영화 데이터 로딩!
+    setTimeout(() => {
+        this.setState({ isLoading: false });
+    }, 5000);
+  }  
+  render() {
+    const { isLoading } = this.state;
+    // We are ready 부분에 영화 데이터를 출력
+    return <div> {isLoading ? 'Loading...' : 'We are ready'} </div>;
+  }    
 }
-
-const fruitILike = [
-  {
-    id : 1,
-    name : 'banana',
-    image: 'http://hschool.co.kr/images/banana.png',
-    rating: 5,
-  },
-  {
-    id : 2,
-    name : 'orange',
-    image: 'http://hschool.co.kr/images/orange.png',
-    rating: 4.9,
-  },
-  {
-    id : 3,
-    name : 'apple',
-    image: 'http://hschool.co.kr/images/apple.png',
-    rating: 4.5,
-  },
-  {
-    id : 4,
-    name : 'melon',
-    image: 'http://hschool.co.kr/images/melon.png',
-    rating: 4.8,
-  },
-];
-
-function App() {
-  return (
-    <div> 
-      {fruitILike.map(dish => (
-        <Fruit key={dish.id} name={dish.name} picture={dish.image} 
-        rating={dish.rating} />
-      ))}
-    </div>
-  );  
-}
-
-Fruit.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number,
-};
 
 export default App;
